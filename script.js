8f7912faaba8e107c6a4cb7f173928ac25f1a0df
Hazza203 <s3383212@student.rmit.edu.au>
2019-03-18 20:41:00 +0800

    Fixed Bug

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var output = document.querySelector("h3");
+//"test"
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
